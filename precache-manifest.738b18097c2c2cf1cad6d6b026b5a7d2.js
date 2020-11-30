3e5b9ff91c7f56997aff4c6b51e3811d4330e2c1
yukd <dhshsktl@naver.com>
2020-11-30 17:38:37 +0900

    Updates

---
+++ precache-manifest.738b18097c2c2cf1cad6d6b026b5a7d2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0119cea9690fd233abd211c7a2f741a7",
+    "url": "/yukd/index.html"
+  },
+  {
+    "revision": "e7c90e8aac77a23748e9",
+    "url": "/yukd/static/css/main.7a95f607.chunk.css"
+  },
+  {
+    "revision": "f0585afd58bab4c12498",
+    "url": "/yukd/static/js/2.9a2180c5.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/yukd/static/js/2.9a2180c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7c90e8aac77a23748e9",
+    "url": "/yukd/static/js/main.da8ec7b8.chunk.js"
+  },
+  {
+    "revision": "b25b8a3d905363749e92",
+    "url": "/yukd/static/js/runtime-main.522b852e.js"
+  },
+  {
+    "revision": "b88924d5d914bf9f3885f87c2ed76477",
+    "url": "/yukd/static/media/sad-icon.b88924d5.png"
+  },
+  {
+    "revision": "b9d7d448a09d6aacb45d21aab2c6fd53",
+    "url": "/yukd/static/media/your photo.b9d7d448.png"
+  }
+]);
